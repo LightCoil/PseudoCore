88928063a091efacfb191053192d8cd29bea6a8e
Rapture <95989103+LightCoil@users.noreply.github.com>
2025-06-17 10:00:00 -0700

    Delete compress.h

--- compress.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPRESS_H
-#define COMPRESS_H
-
-#include <stddef.h>
-
-int compress_page(const char *in, size_t sz, char *out, int lvl);
-int decompress_page(const char *in, size_t sz, char *out);
-
-#endif // COMPRESS_H
